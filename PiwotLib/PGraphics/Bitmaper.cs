41585d72560bb47618fdb8302948d17c9dc7512e
Piotr Wątorski <piotr.watorski@gmail.com>
2019-11-17 17:35:10 +0100

    ResizeToFit

--- PiwotLib/PGraphics/Bitmaper.cs
+++ PiwotLib/PGraphics/Bitmaper.cs
@@ -34,21 +34,43 @@ namespace PiwotToolsLib.PGraphics
         }
 
         /// <summary>
-        /// Returns a new resized instance of the oryginal bitmap.
+        /// Returns a new streched instance of the oryginal bitmap.
         /// </summary>
         /// <param name="bitmap">The oryginal bitmap.</param>
         /// <param name="width">New height.</param>
         /// <param name="height">New width.</param>
         /// <returns></returns>
-        public static Bitmap ResizeBitmap(Bitmap bitmap, int width, int height)
+        public static Bitmap StreachToSize(Bitmap bitmap, int width, int height)
         {
             Bitmap result = new Bitmap(width, height);
             using (Graphics g = Graphics.FromImage(result))
             {
-                g.DrawImage(bitmap, new Rectangle( 0, 0, width, height), 0, 0, bitmap.Width, bitmap.Height, GraphicsUnit.Pixel);
+                g.DrawImage(bitmap, new Rectangle(0, 0, width, height), 0, 0, bitmap.Width, bitmap.Height, GraphicsUnit.Pixel);
             }
 
             return result;
+        }
+
+        /// <summary>
+        /// Returns a new resized instance of the oryginal bitmap, that fits given size.
+        /// </summary>
+        /// <param name="bitmap">The oryginal bitmap.</param>
+        /// <param name="width">Given height.</param>
+        /// <param name="height">Given width.</param>
+        /// <returns></returns>
+        public static Bitmap ResizeToFit(Bitmap bitmap, int width, int height)
+        {
+            float dRatio = (float)width / (float)height;
+            float bRatio = (float)bitmap.Width / (float)bitmap.Height;
+            if (dRatio < bRatio)
+            {
+                return new Bitmap(bitmap, width, (int)(width / bRatio));
+            }
+            else
+            {
+                return new Bitmap(bitmap, (int)(height * bRatio), (int)(height));
+            }
+
         }
 
         public static Bitmap CutColorBits(Bitmap b, Coloring.ColorEncoding colorEncoding)
